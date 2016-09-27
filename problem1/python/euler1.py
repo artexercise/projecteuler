13a767b5d697503a260a062a3275206c83f3b8a1
Joe Jeskiewicz <artexercise@yahoo.com>
2016-09-26 21:52:10 -0400

    Timing different versions of a function

--- problem1/python/euler1.py
+++ problem1/python/euler1.py
@@ -11,9 +11,43 @@ def e1_a(limit):
 			sum = sum + x
 	return sum
 
+def e1_b(limit):
+	'''
+	Trying the same thing with a generator for speed check.  Using the website below to help set up.
+	https://jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
+	'''
+	sum = 0
+	for x in get3or5(1):
+		if x < limit:
+			sum = sum + x
+		else:
+			return sum
+
+def e1_c(limit):
+	'''
+	'''
+	sum = 0
+	for x in range(0,limit):
+		sum += x if x%3 == 0 or x%5 == 0 else 0
+	return sum
+			
+def get3or5(n):
+	while True:
+		if n%3 == 0 or n%5 == 0:
+			yield n
+		n = n + 1
+
 def test_e1_a():
     if not testitem_FunctionTest(e1_a, 10, 23): return
     print("Passed: e1_a")
+
+def test_e1_b():
+    if not testitem_FunctionTest(e1_b, 10, 23): return
+    print("Passed: e1_b")
+
+def test_e1_c():
+    if not testitem_FunctionTest(e1_c, 10, 23): return
+    print("Passed: e1_c")
 
 def testitem_FunctionTest(inFunc, inArg, outResult):
     try:
@@ -26,11 +60,38 @@ def testitem_FunctionTest(inFunc, inArg, outResult):
 # -=-=- -=-=- -=-=- -=-=- -=-=-
 # Test Functions
 # -=-=- -=-=- -=-=- -=-=- -=-=-
+print("--=-- --=-- --=-- --=-- --=--")
+print("--=-- --=-- e1_a  --=-- --=--")
+print("--=-- --=-- --=-- --=-- --=--")
 test_e1_a()
 print("  Result of Function w/ Input 10: {}".format(e1_a(10)))
 print("Result of Function w/ Input 1000: {}".format(e1_a(1000)))
 
 t10 = timeit.timeit(stmt='e1_a(10)', setup='from __main__ import e1_a', number=1000)
 t1000 = timeit.timeit(stmt='e1_a(1000)', setup='from __main__ import e1_a', number=1000)
+print("  Function Time Run 1000 times w/ Input 10: {}".format(t10))
+print("Function Time Run 1000 times w/ Input 1000: {}".format(t1000))
+
+print("--=-- --=-- --=-- --=-- --=--")
+print("--=-- --=-- e1_b  --=-- --=--")
+print("--=-- --=-- --=-- --=-- --=--")
+test_e1_b()
+print("  Result of Function w/ Input 10: {}".format(e1_b(10)))
+print("Result of Function w/ Input 1000: {}".format(e1_b(1000)))
+
+t10 = timeit.timeit(stmt='e1_b(10)', setup='from __main__ import e1_b', number=1000)
+t1000 = timeit.timeit(stmt='e1_b(1000)', setup='from __main__ import e1_b', number=1000)
+print("  Function Time Run 1000 times w/ Input 10: {}".format(t10))
+print("Function Time Run 1000 times w/ Input 1000: {}".format(t1000))
+
+print("--=-- --=-- --=-- --=-- --=--")
+print("--=-- --=-- e1_c  --=-- --=--")
+print("--=-- --=-- --=-- --=-- --=--")
+test_e1_c()
+print("  Result of Function w/ Input 10: {}".format(e1_c(10)))
+print("Result of Function w/ Input 1000: {}".format(e1_c(1000)))
+
+t10 = timeit.timeit(stmt='e1_c(10)', setup='from __main__ import e1_c', number=1000)
+t1000 = timeit.timeit(stmt='e1_c(1000)', setup='from __main__ import e1_c', number=1000)
 print("  Function Time Run 1000 times w/ Input 10: {}".format(t10))
 print("Function Time Run 1000 times w/ Input 1000: {}".format(t1000))
